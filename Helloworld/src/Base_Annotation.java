fb2652410c75bf2ff5cd82c8cbd0097e3d320872
Mahadev <mahadev.is57@gmail.com>
2017-02-07 19:56:36 +0530

    remoove junk jars

---
+++ Helloworld/src/Base_Annotation.java
@@ -0,0 +1,10 @@
+
+
+public class Base_Annotation {
+
+		public static void main(String args[]){
+			
+			System.out.println("I am in main class");
+					
+		}
+}
--- Helloworld/src/Hello.java
+++ Helloworld/src/Hello.java
@@ -1,11 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("hello!");
 
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\mahadev\\git\\Helloworld\\Helloworld\\Lib\\geckodriver.exe");     // Gecko driver path setup
+
+	//	WebDriver driver = new FirefoxDriver();
+		
+		FirefoxDriver driver = new FirefoxDriver();
+
+		driver.get("https:\\www.google.com");
+		
 	}
 
 }
